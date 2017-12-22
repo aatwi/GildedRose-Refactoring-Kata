2ea5e979e0f3e322115991da5585c03311de48c0
aatwi <ahmadatwi23@gmail.com>
2017-12-22 22:16:45 +0200

    [#2] Add 3 new tests in GildedRoseTest
    In order to Increase test coverage of GildedRose
    - Add a test that asserts that the quality of AgedBrie increases with time
    - Add a test that asserts that the quality of Sulfuras never decreases
    - Add a test that asserts that the sell date of Sulfuras never decreases

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,46 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
+    @Ignore
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
+    }
+
+    @Test
+    public void
+    the_quality_of_AgedBrie_should_increase_with_time() {
+        Item[] agedBrie = new Item[] {new Item("Aged Brie", 5, 10)};
+        GildedRose gildedRose = new GildedRose(agedBrie);
+        gildedRose.updateQuality();
+        assertEquals(11, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void
+    the_quality_of_Sulfuras_never_decreases() {
+        Item[] sulfuras = new Item[]{new Item("Sulfuras, Hand of Ragnaros",5, 10)};
+        GildedRose gildedRose = new GildedRose(sulfuras);
+        gildedRose.updateQuality();
+        assertEquals(10, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void
+    the_sell_date_of_Sulfuras_never_decreases() {
+        Item[] sulfuras = new Item[]{new Item("Sulfuras, Hand of Ragnaros",5, 10)};
+        GildedRose gildedRose = new GildedRose(sulfuras);
+        gildedRose.updateQuality();
+        assertEquals(5, gildedRose.items[0].sellIn);
     }
 
 }
