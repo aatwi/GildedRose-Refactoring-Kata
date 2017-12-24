a4ec442f605aa3b15b8f3f7e24f57a336682bfb7
aatwi <ahmadatwi23@gmail.com>
2017-12-24 22:13:58 +0200

    [#4] Use an ItemBuilder to create an instance of the Item class
    In order to have clean tests in the GildedRoseTest
    - Add a new builder class ItemBuilder in the test package
    - Refactor the tests to use the builder instead of the Item Constructor
    - Refactor the assertSellInDateOfGildedRose and assertQualityOfGildedRose to take ItemBuilder instead of Item[]

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.Test;
 
 import static com.gildedrose.GildedRose.*;
+import static com.gildedrose.ItemBuilder.anItemBuilder;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class GildedRoseTest {
@@ -10,145 +11,186 @@ public class GildedRoseTest {
     @Test
     public void
     the_quality_of_AgedBrie_should_increase_with_time() {
-        Item[] agedBrie = new Item[]{new Item(AGED_BRIE, 5, 10)};
-        assertQualityOfGildedRose(agedBrie, 11);
+        assertQualityOfGildedRose(anItemBuilder().name(AGED_BRIE)
+                        .sellInDate(5)
+                        .quality(10),
+                11);
     }
 
     @Test
     public void
     the_quality_of_Sulfuras_never_decreases() {
-        Item[] sulfuras = new Item[]{new Item(SULFURAS, 5, 10)};
-        assertQualityOfGildedRose(sulfuras, 10);
+        assertQualityOfGildedRose(anItemBuilder().name(SULFURAS)
+                        .sellInDate(5)
+                        .quality(10),
+                10);
     }
 
     @Test
     public void
     the_quality_of_Sulfuras_never_decreases_even_when_sell_date_is_over() {
-        Item[] sulfuras = new Item[]{new Item(SULFURAS, -1, 10)};
-        assertQualityOfGildedRose(sulfuras, 10);
+        assertQualityOfGildedRose(anItemBuilder().name(SULFURAS)
+                        .sellInDate(-1)
+                        .quality(10),
+                10);
     }
 
     @Test
     public void
     the_quality_of_AgedBrie_is_never_above_50() {
-        Item[] items = new Item[]{new Item(AGED_BRIE, 5, 50)};
-        assertQualityOfGildedRose(items, 50);
+        assertQualityOfGildedRose(anItemBuilder().name(AGED_BRIE)
+                        .sellInDate(5)
+                        .quality(50),
+                50);
     }
 
     @Test
     public void
     the_quality_of_Sulfuras_is_never_above_50() {
-        Item[] items = new Item[]{new Item(SULFURAS, 5, 50)};
-        assertQualityOfGildedRose(items, 50);
+        assertQualityOfGildedRose(anItemBuilder().name(SULFURAS)
+                        .sellInDate(5)
+                        .quality(50),
+                50);
     }
 
     @Test
     public void
     the_quality_of_Concert_is_never_above_50_when_sell_date_is_greater_than_11() {
-        Item[] items = new Item[]{new Item(CONCERT, 15, 50)};
-        assertQualityOfGildedRose(items, 50);
+        assertQualityOfGildedRose(anItemBuilder().name(CONCERT)
+                        .sellInDate(15)
+                        .quality(50),
+                50);
     }
 
     @Test
     public void
     the_quality_of_Concert_is_never_above_50_when_sell_date_is_between_6_and_11() {
-        Item[] items = new Item[]{new Item(CONCERT, 10, 49)};
-        assertQualityOfGildedRose(items, 50);
+        assertQualityOfGildedRose(anItemBuilder().name(CONCERT)
+                        .sellInDate(10)
+                        .quality(49),
+                50);
     }
 
     @Test
     public void
     the_quality_of_Concert_is_never_above_50_when_sell_date_is_between_0_and_6() {
-        Item[] items = new Item[]{new Item(CONCERT, 4, 48)};
-        assertQualityOfGildedRose(items, 50);
+        assertQualityOfGildedRose(anItemBuilder().name(CONCERT)
+                        .sellInDate(4)
+                        .quality(48),
+                50);
     }
 
     @Test
     public void
     the_quality_of_any_other_item_is_never_above_50() {
-        Item[] items = new Item[]{new Item("Other Item", 5, 50)};
-        assertQualityOfGildedRose(items, 49);
+        assertQualityOfGildedRose(anItemBuilder().name("Other Item")
+                        .sellInDate(5)
+                        .quality(50),
+                49);
     }
 
     @Test
     public void
     the_quality_of_backstage_increase_by_2_when_sell_date_is_between_6_and_10() {
-        Item[] items = new Item[]{new Item(CONCERT, 7, 5)};
-        assertQualityOfGildedRose(items, 7);
+        assertQualityOfGildedRose(anItemBuilder().name(CONCERT)
+                        .sellInDate(7)
+                        .quality(5),
+                7);
     }
 
     @Test
     public void
     the_quality_of_backstage_increases_by_3_when_sell_date_is_between_0_and_6() {
-        Item[] items = new Item[]{new Item(CONCERT, 5, 5)};
-        assertQualityOfGildedRose(items, 8);
+        assertQualityOfGildedRose(anItemBuilder().name(CONCERT)
+                        .sellInDate(5)
+                        .quality(5),
+                8);
     }
 
     @Test
     public void
     the_quality_of_backstage_drops_to_zero_after_concert() {
-        Item[] items = new Item[]{new Item(CONCERT, 0, 5)};
-        assertQualityOfGildedRose(items, 0);
+        assertQualityOfGildedRose(anItemBuilder().name(CONCERT)
+                        .sellInDate(0)
+                        .quality(5),
+                0);
     }
 
     @Test
     public void
     the_quality_of_AgedBrie_should_increase_by_2_when_sell_date_is_less_than_zero() {
-        Item[] agedBrie = new Item[]{new Item(AGED_BRIE, 0, 10)};
-        assertQualityOfGildedRose(agedBrie, 12);
+        assertQualityOfGildedRose(anItemBuilder().name(AGED_BRIE)
+                        .sellInDate(0)
+                        .quality(10),
+                12);
     }
 
     @Test
     public void
     the_quality_of_other_items_decreases_by_2_when_sell_date_is_less_than_zero() {
-        Item[] agedBrie = new Item[]{new Item("Other Items", 0, 10)};
-        assertQualityOfGildedRose(agedBrie, 8);
+        assertQualityOfGildedRose(anItemBuilder().name("Other Items")
+                        .sellInDate(0)
+                        .quality(10),
+                8);
     }
 
     @Test
     public void
     the_quality_of_any_other_item_is_never_below_0() {
-        Item[] items = new Item[]{new Item("Other Item", 0, 0)};
-        assertQualityOfGildedRose(items, 0);
+        assertQualityOfGildedRose(anItemBuilder().name("Other Item")
+                        .sellInDate(0)
+                        .quality(0),
+                0);
     }
 
     @Test
     public void
     the_sell_date_of_Sulfuras_never_decreases() {
-        Item[] sulfuras = new Item[]{new Item(SULFURAS, 5, 10)};
-        assertSellInDateOfGildedRose(sulfuras, 5);
+        assertSellInDateOfGildedRose(anItemBuilder().name(SULFURAS)
+                        .sellInDate(5)
+                        .quality(10),
+                5);
     }
 
     @Test
     public void
     the_sell_date_of_AgedBrie_decreases_by_one() {
-        Item[] items = new Item[]{new Item(AGED_BRIE, 5, 50)};
-        assertSellInDateOfGildedRose(items, 4);
+        assertSellInDateOfGildedRose(anItemBuilder().name(AGED_BRIE)
+                        .sellInDate(5)
+                        .quality(50),
+                4);
     }
 
     @Test
     public void
     the_sell_date_of_Concert_decreases_by_one() {
-        Item[] items = new Item[]{new Item(CONCERT, 5, 50)};
-        assertSellInDateOfGildedRose(items, 4);
+        assertSellInDateOfGildedRose(anItemBuilder().name(CONCERT)
+                        .sellInDate(5)
+                        .quality(50),
+                4);
     }
 
     @Test
     public void
     the_sell_date_of_any_other_item_decreases_by_one() {
-        Item[] items = new Item[]{new Item("Other Item", 5, 50)};
-        assertSellInDateOfGildedRose(items, 4);
+        assertSellInDateOfGildedRose(anItemBuilder().name("Other Item")
+                        .sellInDate(5)
+                        .quality(50),
+                4);
     }
 
-    private void assertSellInDateOfGildedRose(Item[] items, int expected) {
+    private void assertSellInDateOfGildedRose(ItemBuilder item, int expected) {
+        Item[] items = new Item[]{item.build()};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         assertThat(gildedRose.items[0].sellIn).isEqualTo(expected);
     }
 
-    private void assertQualityOfGildedRose(Item[] items, int expected) {
+    private void assertQualityOfGildedRose(ItemBuilder item, int expected) {
+        Item[] items = new Item[]{item.build()};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         assertThat(gildedRose.items[0].quality).isEqualTo(expected);
     }
+
 }
---
+++ Java/src/test/java/com/gildedrose/ItemBuilder.java
@@ -0,0 +1,31 @@
+package com.gildedrose;
+
+public class ItemBuilder {
+
+    private String name;
+    private int sellInDate;
+    private int quality;
+
+    public static ItemBuilder anItemBuilder() {
+        return new ItemBuilder();
+    }
+
+    public ItemBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ItemBuilder sellInDate(int sellInDate) {
+        this.sellInDate = sellInDate;
+        return this;
+    }
+
+    public ItemBuilder quality(int quality) {
+        this.quality = quality;
+        return this;
+    }
+
+    public Item build() {
+        return new Item(name, sellInDate, quality);
+    }
+}
