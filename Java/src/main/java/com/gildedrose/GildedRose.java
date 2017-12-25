f88bd29074610217abeff58ee05ce7b55fe33913
aatwi <ahmadatwi23@gmail.com>
2017-12-25 15:11:51 +0200

    [#5] Use the visitor in the case of AgedBrie
    In order to refactor and clean the code of GildedRose
    - Implement the logic for AgedBrie in the corresponding visit method in ItemVisitor
    - Use the visitor pattern in the GildedRose in the case of AgedBrie

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,51 +22,56 @@ public final class GildedRose {
     }
 
     public void updateQuality() {
-        for (Item item : itemsList) {
-            if (!item.name.equals(AGED_BRIE) && !item.name.equals(CONCERT)) {
-                if (item.quality > MIN_QUALITY) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+        ItemVisitor itemVisitor = new ItemVisitor();
+        for (ItemVisitable item : itemsList) {
+            if (item instanceof AgedBrie) {
+                item.accept(itemVisitor);
             } else {
-                if (item.quality < MAX_QUALITY) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(CONCERT)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
+                if (!item.name.equals(AGED_BRIE) && !item.name.equals(CONCERT)) {
+                    if (item.quality > MIN_QUALITY) {
+                        if (!item.name.equals(SULFURAS)) {
+                            item.quality = item.quality - 1;
                         }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < MIN_SELL_IN_DATE) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(CONCERT)) {
-                        if (item.quality > MIN_QUALITY) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
                 } else {
                     if (item.quality < MAX_QUALITY) {
                         item.quality = item.quality + 1;
+
+                        if (item.name.equals(CONCERT)) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < MAX_QUALITY) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < MAX_QUALITY) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (!item.name.equals(SULFURAS)) {
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < MIN_SELL_IN_DATE) {
+                    if (!item.name.equals(AGED_BRIE)) {
+                        if (!item.name.equals(CONCERT)) {
+                            if (item.quality > MIN_QUALITY) {
+                                if (!item.name.equals(SULFURAS)) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        } else {
+                            item.quality = item.quality - item.quality;
+                        }
+                    } else {
+                        if (item.quality < MAX_QUALITY) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             }
--- Java/src/main/java/com/gildedrose/ItemVisitor.java
+++ Java/src/main/java/com/gildedrose/ItemVisitor.java
@@ -1,10 +1,22 @@
 package com.gildedrose;
 
+import static com.gildedrose.GildedRose.MAX_QUALITY;
+import static com.gildedrose.GildedRose.MIN_SELL_IN_DATE;
+
 public class ItemVisitor implements Visitor {
 
     @Override
     public void visit(AgedBrie agedBrie) {
+        if (agedBrie.quality < MAX_QUALITY) {
+            agedBrie.quality = agedBrie.quality + 1;
+        }
+        agedBrie.sellIn = agedBrie.sellIn - 1;
 
+        if (agedBrie.sellIn < MIN_SELL_IN_DATE) {
+            if (agedBrie.quality < MAX_QUALITY) {
+                agedBrie.quality = agedBrie.quality + 1;
+            }
+        }
     }
 
     @Override
